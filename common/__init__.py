5bd6232af63d38ccc8571d7e22363bad7ab838c1
domfeng <domfeng@163.com>
2024-02-02 16:59:54 +0800

    初始化po结构

---
+++ common/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+# coding=utf-8
+# @Time: 2024/2/2 16:59
+# @Author: fengjianguang
---
+++ page/__init__.py
@@ -0,0 +1,4 @@
+#! /usr/bin/python3
+# coding=utf-8
+# @Time: 2024/2/2 16:52
+# @Author: fengjianguang
