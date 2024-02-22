cef0a750f20fefbea456024f9cae864f8bf812c1
domfeng <domfeng@163.com>
2024-02-22 17:27:08 +0800

    获取数据库信息方法

--- common/yaml_config.py
+++ common/yaml_config.py
@@ -5,15 +5,21 @@
 import yaml
 from common.tools import get_project_path, sep
 
+
 class GetConf:
     def __init__(self):
-        with open(get_project_path()+sep(["config", "environment.yaml"], add_sep_before=True), "r",
+        with open(get_project_path() + sep(["config", "environment.yaml"], add_sep_before=True), "r",
                   encoding="utf-8") as env_file:
             self.env = yaml.load(env_file, Loader=yaml.FullLoader)
-            # print(self.env)
+            print(self.env)
 
     def get_username_password(self):
         return self.env["username"], self.env["password"]
 
+    def get_db(self):
+        return self.env["mysql"]
+
+
 if __name__ == "__main__":
     print(GetConf().get_username_password())
+    print(GetConf().get_db())
