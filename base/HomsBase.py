1ffa36030150bb57ee7f2745ab4415595504388c
domfeng <domfeng@163.com>
2024-02-23 17:09:57 +0800

    preceding-sibling、ancestor定位方法

--- base/HomsBase.py
+++ base/HomsBase.py
@@ -31,3 +31,17 @@ class HomeBase:
         :return:
         """
         return "//div[text()='我的日历']/following-sibling::div"
+
+    def home_user_avatar(self):
+        """
+        首页用户头像大图
+        :return:
+        """
+        return "//span[contains(text(),'欢迎您')]/parent::div/preceding-sibling::div//img"
+
+    def home_user_avatar_2(self):
+        """
+        首页用户头像大图2
+        :return:
+        """
+        return "//span[text()='我的地址']/ancestor::div[@class='first_card']/div[contains(@class,'avatar')]//img"
